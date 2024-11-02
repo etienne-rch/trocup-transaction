7737772c1de1e7f82fc99b904fef5a87a112a9a2
niiko <nicolas.rocagel@gmail.com>
2024-11-02 15:22:27 +0100

    🚚 Change endpoints prefixes

--- routes/transaction_routes.go
+++ routes/transaction_routes.go
@@ -8,18 +8,19 @@ import (
 )
 
 func TransactionRoutes(app *fiber.App) {
-	// Route publique pour vérifier la santé de l'API
-	app.Get("/health", handlers.HealthCheck)
 
-	// Groupe des routes de l'API
-	api := app.Group("/api")
+	// Routes publiques : accessibles sans authentification
+	public := app.Group("/api")
 
-	// Appliquer le middleware Clerk aux routes sensibles
-	api.Use(middleware.ClerkAuthMiddleware)
+	public.Get("/health", handlers.HealthCheck)
 
-	api.Post("/transactions", handlers.CreateTransaction)
-	api.Get("/transactions/:id", handlers.GetTransaction)
-	api.Get("/transactions", handlers.GetAllTransactions)
-	api.Put("/transactions/:id", handlers.UpdateTransaction)
-	api.Delete("/transactions/:id", handlers.DeleteTransaction)
+
+	// Routes protégées : accessibles uniquement avec authentification
+	protected := app.Group("/api/protected", middleware.ClerkAuthMiddleware)
+
+	protected.Post("/transactions", handlers.CreateTransaction)
+	protected.Get("/transactions/:id", handlers.GetTransaction)
+	protected.Get("/transactions", handlers.GetAllTransactions)
+	protected.Put("/transactions/:id", handlers.UpdateTransaction)
+	protected.Delete("/transactions/:id", handlers.DeleteTransaction)
 }
